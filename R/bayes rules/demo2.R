37d16d9709550b0b2deb0678a92b1680cbcc7c4c
ES\mcondedesimon <mcondedesimon@deloittedigital.es>
2022-05-31 16:52:59 +0200

    demo2 bayesrules

---
+++ R/bayes rules/demo2.R
@@ -0,0 +1,57 @@
+library(bayesrules)
+library(tidyverse)
+library(lme4)
+
+# Load data
+data(cherry_blossom_sample)
+running <- cherry_blossom_sample
+
+# Remove NAs
+running <- running %>% 
+  select(runner, age, net) %>% 
+  na.omit()
+
+
+# POOLED ------------------------------------------------------------------
+
+pooled_lm <- lm(net ~ age, running)
+summary(pooled_lm)
+
+ggplot(running, aes(age, net)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE)
+
+
+
+# NO POOLING --------------------------------------------------------------
+
+no_pooling_lm <- lm(net ~ age:runner, running)
+summary(no_pooling_lm)
+
+running %>% filter(runner %in% 1:10) %>% 
+  ggplot(aes(age, net, color = runner)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE)
+
+
+# MULTILEVEL --------------------------------------------------------------
+
+multilvl_lm <- lmer(net ~ age + (age | runner), running)
+coef(multilvl_lm)
+
+
+coef_multilvl_lm <- coef(multilvl_lm)$runner %>% 
+  as_tibble() %>% 
+  rename(intercept = `(Intercept)`, slope = age) %>% 
+  mutate(runner = factor(1:nrow(.)), .before = 1)
+
+
+running %>% filter(runner %in% 1:10) %>% 
+  ggplot(aes(age, net)) +
+  geom_point() +
+  geom_smooth(color = "black", size = 2, method = "lm", se = FALSE) +
+  geom_point(aes(color = runner)) +
+  geom_smooth(aes(color = runner), method = "lm", se = FALSE) +  
+  geom_abline(data = coef_multilvl_lm  %>% slice(1:10), 
+              mapping = aes(intercept = intercept, slope = slope, color = runner), 
+              linetype = 2, size = 1.2)
